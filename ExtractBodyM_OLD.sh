b5f2a79dc6095260072b44b0706919f0d7ea9ba7
Deap Bhandal <deapbhandal@gmail.com>
2020-02-03 20:25:55 -0800

    Made ExtractBodyM.sh dynamic

--- ExtractBodyM_OLD.sh
+++ ExtractBodyM_OLD.sh
@@ -1,24 +1,12 @@
 #!/bin/bash 
-
-#tail -n +2 takes out header in input file
+#creating the BodyM.csv file from Pacifici data
+#tail -n +2 takes out header in Pacifici file
 #cut -d ";" -f 2-6 extracts columns 2-6 
 #tr -s ";" " " replaces the ; delimiter with a space
 #sort -r -n -k 6 sorts the output in reverse order with respect to body mass
-#> overwrites this data to output file
-
-## delimiter must be surrounded in single quotes when typing third argument
-
-tail -n +2 $1 | cut -d "$3" -f 2-6 | tr -s "$3" " " | sort -r -n -k 6 > $2
-echo "The output file has been created"
-
-
-
-
-
-
-
-
-
+#> overwrites this data to BodyM.csv
+tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > BodyM.csv
+echo "The BodyM.csv file has been created"
 
 #Question 6 (running without typing bash)
 #cd to directory that holds the script: in this case eeb-c177-scripts
